8d1928c78aee01060984bc48f4ec92dc31c26dc8
Bart Ledoux <ledouxb@gmail.com>
2017-12-26 17:32:48 +0100

    fix code climate warnings

--- build/utils.js
+++ build/utils.js
@@ -9,6 +9,30 @@ exports.assetsPath = function (_path) {
   return path.posix.join(assetsSubDirectory, _path)
 }
 
+// generate loader string to be used with extract text plugin
+function generateLoaders (options, loader, loaderOptions) {
+  var loaders = [cssLoader]
+  if (loader) {
+    loaders.push({
+      loader: loader + '-loader',
+      options: Object.assign({}, loaderOptions, {
+        sourceMap: options.sourceMap
+      })
+    })
+  }
+
+  // Extract CSS when that option is specified
+  // (which is the case during production build)
+  if (options.extract) {
+    return ExtractTextPlugin.extract({
+      use: loaders,
+      fallback: 'vue-style-loader'
+    })
+  } else {
+    return ['vue-style-loader'].concat(loaders)
+  }
+}
+
 exports.cssLoaders = function (options) {
   options = options || {}
 
@@ -20,39 +44,15 @@ exports.cssLoaders = function (options) {
     }
   }
 
-  // generate loader string to be used with extract text plugin
-  function generateLoaders (loader, loaderOptions) {
-    var loaders = [cssLoader]
-    if (loader) {
-      loaders.push({
-        loader: loader + '-loader',
-        options: Object.assign({}, loaderOptions, {
-          sourceMap: options.sourceMap
-        })
-      })
-    }
-
-    // Extract CSS when that option is specified
-    // (which is the case during production build)
-    if (options.extract) {
-      return ExtractTextPlugin.extract({
-        use: loaders,
-        fallback: 'vue-style-loader'
-      })
-    } else {
-      return ['vue-style-loader'].concat(loaders)
-    }
-  }
-
   // https://vue-loader.vuejs.org/en/configurations/extract-css.html
   return {
-    css: generateLoaders(),
-    postcss: generateLoaders(),
-    less: generateLoaders('less'),
-    sass: generateLoaders('sass', { indentedSyntax: true }),
-    scss: generateLoaders('sass'),
-    stylus: generateLoaders('stylus'),
-    styl: generateLoaders('stylus')
+    css: generateLoaders(options),
+    postcss: generateLoaders(options),
+    less: generateLoaders(options, 'less'),
+    sass: generateLoaders(options, 'sass', { indentedSyntax: true }),
+    scss: generateLoaders(options, 'sass'),
+    stylus: generateLoaders(options, 'stylus'),
+    styl: generateLoaders(options, 'stylus')
   }
 }
 
