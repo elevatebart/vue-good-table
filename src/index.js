03c65d37b4e051cc276d471956b039a00289a547
Bart Ledoux <ledouxb@gmail.com>
2017-12-26 17:56:10 +0100

    adapt to linters

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import GoodTable from './components/Table.vue';
+import GoodTable from './components/Table.vue'
 
 const GoodTablePlugin = {
   install: function (Vue, options) {
-    Vue.component(GoodTable.name, GoodTable);
-  },
-};
+    Vue.component(GoodTable.name, GoodTable)
+  }
+}
 
 // Automatic installation if Vue has been added to the global scope.
 if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(GoodTablePlugin);
+  window.Vue.use(GoodTablePlugin)
 }
 
-export default GoodTablePlugin;
+export default GoodTablePlugin
 export {
    GoodTable as VueGoodTable
-};
+}
 
